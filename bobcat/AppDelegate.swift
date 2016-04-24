0e54feca85c81ca8abe53bcd1a887f8110725ee7
Tom Hutchings <goerofmuns@gmail.com>
2016-04-24 12:58:18 +0100

    Initial Commit

---
+++ bobcat/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  bobcat
+//
+//  Created by Tom Hutchings on 24/04/2016.
+//  Copyright © 2016 Tom Hutchings. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ bobcat/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  bobcat
+//
+//  Created by Tom Hutchings on 24/04/2016.
+//  Copyright © 2016 Tom Hutchings. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
